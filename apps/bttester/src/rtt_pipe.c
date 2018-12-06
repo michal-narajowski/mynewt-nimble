66b7e12f7279192a8b9b7a8ccc06c633bd0bc4fa
Michał Narajowski <michal.narajowski@codecoup.pl>
2018-12-06 10:40:43 +0100

    apps/bttester: Use dedicated RTT buffer for BTP communication

--- apps/bttester/src/rtt_pipe.c
+++ apps/bttester/src/rtt_pipe.c
@@ -17,6 +17,7 @@
  * under the License.
  */
 
+#include <console/console.h>
 #include "syscfg/syscfg.h"
 
 #if MYNEWT_VAL(BTTESTER_PIPE_RTT)
@@ -34,9 +35,28 @@ static u8_t *recv_buf;
 static size_t recv_buf_len;
 static size_t recv_off;
 
+static uint8_t rtt_buf_up[MYNEWT_VAL(BTTESTER_RTT_BUFFER_SIZE_UP)];
+static uint8_t rtt_buf_down[MYNEWT_VAL(BTTESTER_RTT_BUFFER_SIZE_DOWN)];
+static int rtt_index_up, rtt_index_down;
+
 #define RTT_INPUT_POLL_INTERVAL_MIN     10 /* ms */
 #define RTT_INPUT_POLL_INTERVAL_STEP    10 /* ms */
 #define RTT_INPUT_POLL_INTERVAL_MAX     250 /* ms */
+
+static int rtt_pipe_get_char(unsigned int index)
+{
+    char c;
+    int r;
+
+    r = (int)SEGGER_RTT_Read(index, &c, 1u);
+    if (r == 1) {
+        r = (int)(unsigned char)c;
+    } else {
+        r = -1;
+    }
+
+    return r;
+}
 
 static void
 rtt_pipe_poll_func(void *arg)
@@ -46,7 +66,7 @@ rtt_pipe_poll_func(void *arg)
     int avail = recv_buf_len - recv_off;
 
     if (key < 0) {
-        key = SEGGER_RTT_GetKey();
+        key = rtt_pipe_get_char((unsigned int) rtt_index_down);
     }
 
     if (key < 0) {
@@ -57,13 +77,13 @@ rtt_pipe_poll_func(void *arg)
             recv_buf[recv_off] = (u8_t) key;
             recv_off++;
             avail = recv_buf_len - recv_off;
-            key = SEGGER_RTT_GetKey();
+            key = rtt_pipe_get_char((unsigned int) rtt_index_down);
         }
 
         /*
-	 * Call application callback with received data. Application
-	 * may provide new buffer or alter data offset.
-	 */
+         * Call application callback with received data. Application
+         * may provide new buffer or alter data offset.
+         */
         recv_buf = app_cb(recv_buf, &recv_off);
 
         itvl_ms = RTT_INPUT_POLL_INTERVAL_MIN;
@@ -75,10 +95,9 @@ rtt_pipe_poll_func(void *arg)
 int
 bttester_pipe_send(const u8_t *data, int len)
 {
-    SEGGER_RTT_Write(0, data, len);
+    SEGGER_RTT_Write((unsigned int) rtt_index_up, data, (unsigned int) len);
     return 0;
 }
-
 
 void
 bttester_pipe_register(u8_t *buf, size_t len, bttester_pipe_recv_cb cb)
@@ -91,6 +110,25 @@ bttester_pipe_register(u8_t *buf, size_t len, bttester_pipe_recv_cb cb)
 int
 bttester_pipe_init(void)
 {
+    rtt_index_up = SEGGER_RTT_AllocUpBuffer(MYNEWT_VAL(BTTESTER_RTT_BUFFER_NAME),
+                                            rtt_buf_up, sizeof(rtt_buf_up),
+                                            SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+
+    if (rtt_index_up < 0) {
+        return -1;
+    }
+
+    rtt_index_down = SEGGER_RTT_AllocDownBuffer(MYNEWT_VAL(BTTESTER_RTT_BUFFER_NAME),
+                                                rtt_buf_down, sizeof(rtt_buf_down),
+                                                SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+
+    if (rtt_index_down < 0) {
+        return -1;
+    }
+
+    console_printf("Using up-buffer #%d\n", rtt_index_up);
+    console_printf("Using down-buffer #%d\n", rtt_index_down);
+
     os_cputime_timer_init(&rtt_timer, rtt_pipe_poll_func, NULL);
     os_cputime_timer_relative(&rtt_timer, 200000);
     return 0;
